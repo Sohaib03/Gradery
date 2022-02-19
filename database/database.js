8fcf49883b1f3111f5fb8ff14024ef3d1e032207
Sohaib03 <sohaibbinmusa@gmail.com>
2022-02-19 19:58:11 +0600

    Assignment Create Complete

--- database/database.js
+++ database/database.js
@@ -4,96 +4,97 @@ require("dotenv").config();
 oracledb.autoCommit = true;
 
 const database_options = {
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  connectString: process.env.DB_CONNECTSTRING,
+	user: process.env.DB_USER,
+	password: process.env.DB_PASS,
+	connectString: process.env.DB_CONNECTSTRING,
 };
 
 async function startup() {
-  console.log("Initializing database");
-  const pool = await oracledb.createPool(database_options);
-  connection = await oracledb.getConnection();
-  connection.close();
-  await initializeProcedures();
-  console.log("Database Initialized Successfully");
+	console.log("Initializing database");
+	const pool = await oracledb.createPool(database_options);
+	connection = await oracledb.getConnection();
+	connection.close();
+	await initializeProcedures();
+	console.log("Database Initialized Successfully");
 }
 
 async function shutdown() {
-  console.log("Shutting down database");
-  try {
-    await oracledb.getPool().close(10);
-    console.log("Pools Closed");
-  } catch (err) {
-    console.log("Error : function shutdown()");
-  }
+	console.log("Shutting down database");
+	try {
+		await oracledb.getPool().close(10);
+		console.log("Pools Closed");
+	} catch (err) {
+		console.log("Error : function shutdown()");
+	}
 }
 
 async function execute(sqlCommand, bindParams, options) {
-  let connection, results;
+	let connection, results;
 
-  try {
-    connection = await oracledb.getConnection();
-    results = await connection.execute(sqlCommand, bindParams, options);
-  } catch (err) {
-    console.log(
-      "Error : function execute(" +
-        sqlCommand +
-        ") [Message : " +
-        err.message +
-        " ]"
-    );
-  } finally {
-    if (connection) {
-      try {
-        await connection.close();
-      } catch (err) {
-        console.log(
-          "Error : function execute() while closing connection : " + err.message
-        );
-      }
-    }
-  }
-  return results;
+	try {
+		connection = await oracledb.getConnection();
+		results = await connection.execute(sqlCommand, bindParams, options);
+	} catch (err) {
+		console.log(
+			"Error : function execute(" +
+				sqlCommand +
+				") [Message : " +
+				err.message +
+				" ]"
+		);
+	} finally {
+		if (connection) {
+			try {
+				await connection.close();
+			} catch (err) {
+				console.log(
+					"Error : function execute() while closing connection : " +
+						err.message
+				);
+			}
+		}
+	}
+	return results;
 }
 
 async function executeMany(sqlCommand, bindParams, options) {
-  let connection;
+	let connection;
 
-  try {
-    connection = await oracledb.getConnection();
-  } catch (err) {
-    console.log(
-      "Error : function executeMany(" +
-        sqlCommand +
-        ") [Message : " +
-        err.message +
-        " ]"
-    );
-  } finally {
-    if (connection) {
-      try {
-        await connection.close();
-      } catch (err) {
-        console.log(
-          "Error : function executeMany() while closing connection : " +
-            err.message
-        );
-      }
-    }
-  }
+	try {
+		connection = await oracledb.getConnection();
+	} catch (err) {
+		console.log(
+			"Error : function executeMany(" +
+				sqlCommand +
+				") [Message : " +
+				err.message +
+				" ]"
+		);
+	} finally {
+		if (connection) {
+			try {
+				await connection.close();
+			} catch (err) {
+				console.log(
+					"Error : function executeMany() while closing connection : " +
+						err.message
+				);
+			}
+		}
+	}
 }
 
 async function initializeProcedures() {
-  const team_notification = `create or replace procedure create_team_notification(team_id in NUMBER, title in VARCHAR2, content in VARCHAR2)
+	const team_notification = `create or replace procedure create_team_notification(team_id in NUMBER, title in VARCHAR2, content in VARCHAR2)
 IS
     nid Number;
 begin
     insert into NOTIFICATION (TITLE, CONTENT) VALUES (title, content) returning NOTIFICATION_ID into nid;
     insert into NOTIFICATION_RECEIVED_BY_TEAM (TEAM_ID, NOTIFICATION_ID) values (team_id, nid);
 end;`;
-  await execute(team_notification, {}, options);
+	await execute(team_notification, {}, options);
 
-  const create_team = `
+	const create_team = `
 create or replace procedure create_team(team_name in varchar2, user_id in Number, team_code in varchar2, team_desc in varchar2, course_id in varchar2)
 IS
     tid Number;
@@ -101,21 +102,35 @@ begin
     insert into TEAMS (TEAM_NAME, CREATED_BY, TEAM_CODE, TEAM_DESC, COURSE_ID) values (team_name, user_id, team_code, team_desc, course_id) returning TEAM_ID into tid;
     insert into DISCUSSION (TITLE, BODY, TEAM_ID, STATUS) VALUES ('GENERAL', 'General Discussion', tid, 1);
 end;`;
+	const create_assignment = `
+create or replace procedure create_assignment(team_id_var in Number, title in varchar2, assignment_desc in varchar2,
+ created_by in varchar2, file_path in varchar2, deadline in DATE)
+is
+    ass_id Number;
+begin
+    insert into ASSIGNMENTS (ASSIGNMENT_TITLE, ASSIGNMENT_DESC, CREATED_BY, TEAM_ID, DEADLINE, FILE_URL) values
+        (title, assignment_desc, created_by, team_id_var, deadline, file_path) returning ASSIGNMENT_ID into ass_id;
+    insert into ASSIGNED_TO (ASSIGNMENT_ID, STUDENT_ID, SUBMISSION_STATUS)
+        select ass_id, P.USER_ID, 0 from PARTICIPANT P
+            where P.ROLE = 'general' and P.TEAM_ID = team_id_var;
+end;
+    `;
 
-  await execute(create_team, {}, options);
+	await execute(create_team, {}, options);
+	await execute(create_assignment, {}, options);
 
-  console.log("Procedure Initialized");
+	console.log("Procedure Initialized");
 }
 
 const options = {
-  outFormat: oracledb.OUT_FORMAT_OBJECT,
+	outFormat: oracledb.OUT_FORMAT_OBJECT,
 };
 
 module.exports = {
-  startup,
-  shutdown,
-  execute,
-  executeMany,
-  options,
-  initializeProcedures,
+	startup,
+	shutdown,
+	execute,
+	executeMany,
+	options,
+	initializeProcedures,
 };
--- routes/assignment.js
+++ routes/assignment.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const auth = require("../routes/auth");
 const user_middleware = require("../middlewares/user_middleware");
+const assignmentDB = require("../database/assignments");
 
 router
 	.route("/create/:team_id/:team_code")
@@ -30,32 +31,40 @@ router
 			const title = req.body.title;
 			const desc = req.body.desc;
 			const deadline_date = req.body.deadline;
-			let file;
+			let file, file_name;
 			if (req.files) file = req.files.file;
 			const [d_date, d_time] = deadline_date.split("T");
 			console.log({ title, desc, d_date, d_time, file });
-			if (file)
-				file.mv(
-					"./uploads/" +
-						req.session.username +
-						"_" +
-						new Date().getTime() +
-						file.name,
-					(err) => {
-						if (err) {
-							req.session.notification = {
-								status: "is-danger is-light",
-								content: "Error while uploading file",
-							};
-						} else {
-							req.session.notification = {
-								status: "is-success is-light",
-								content: "Successfully Created assignment",
-							};
-						}
-					}
-				);
 
+			if (file) {
+				file_name =
+					"./uploads/" +
+					req.session.username +
+					"_" +
+					new Date().getTime() +
+					file.name;
+				file.mv(file_name, (err) => {
+					if (err) {
+						req.session.notification = {
+							status: "is-danger is-light",
+							content: "Error while uploading file",
+						};
+					} else {
+						req.session.notification = {
+							status: "is-success is-light",
+							content: "Successfully Created assignment",
+						};
+					}
+				});
+			}
+			assignmentDB.createAssignment(
+				team_id,
+				title,
+				desc,
+				req.session.user_id,
+				file_name,
+				d_date + " " + d_time
+			);
 			res.redirect("/teams/code/" + team_code);
 		}
 	);
