5d15fdf0d296b9782cdc0c8d74a53219a1e25be0
Sohaib03 <sohaibbinmusa@gmail.com>
2022-02-23 11:35:50 +0600

    Context Fix

--- routes/grading.js
+++ routes/grading.js
@@ -6,120 +6,131 @@ const assignmentDB = require("../database/assignments");
 const compression = require("../utils/compression");
 
 router.route("/:ass_id").get(auth.authMiddleware, async (req, res) => {
-    const ass_id = req.params.ass_id;
-    const ass_info = await assignmentDB.getAssignmentById(ass_id);
-    if (ass_info.length === 0) {
-        res.redirect("/");
-        return;
-    }
+	const ass_id = req.params.ass_id;
+	const ass_info = await assignmentDB.getAssignmentById(ass_id);
+	if (ass_info.length === 0) {
+		res.redirect("/");
+		return;
+	}
 
-    const team_id = ass_info[0].TEAM_ID;
-    const team_info = await teams.getTeamInfo(team_id);
+	const team_id = ass_info[0].TEAM_ID;
+	const team_info = await teams.getTeamInfo(team_id);
 
-    if (team_info.length === 0) {
-        res.redirect("/");
-        return;
-    }
+	if (team_info.length === 0) {
+		res.redirect("/");
+		return;
+	}
 
-    const team_role = await teams.checkUserInTeam(req.session.user_id, team_id);
+	const team_role = await teams.checkUserInTeam(req.session.user_id, team_id);
 
-    if (team_role.length === 0) {
-        res.redirect("/");
-        return;
-    }
+	if (team_role.length === 0) {
+		res.redirect("/");
+		return;
+	}
 
-    if (team_role[0].ROLE === "student") {
-        res.redirect("/assignment/" + ass_id);
-        return;
-    }
+	if (team_role[0].ROLE === "student") {
+		res.redirect("/assignment/" + ass_id);
+		return;
+	}
 
-    const team_code = team_info[0].TEAM_CODE;
-    const team_name = team_info[0].TEAM_NAME;
-    const team_desc = team_info[0].TEAM_DESC;
+	const team_code = team_info[0].TEAM_CODE;
+	const team_name = team_info[0].TEAM_NAME;
+	const team_desc = team_info[0].TEAM_DESC;
 
-    let allSubs = await assignmentDB.allSubs(ass_id);
-    let subData = await assignmentDB.getSubmissionData(ass_id);
-    console.log({ subData });
+	let allSubs = await assignmentDB.allSubs(ass_id);
+	let subData = await assignmentDB.getSubmissionData(ass_id);
+	console.log({ subData });
 
-    let context = {
-        title: "Create Assignment",
-        username: req.session.username,
-        role: req.session.role,
-        submissions: allSubs,
-        ass_info: ass_info[0],
-        sub_info: subData[0],
-        team_code: team_code,
-        team_name: team_name,
-        team_desc: team_desc,
-    };
-    res.render("grading", context);
+	let context = {
+		title: "Create Assignment",
+		username: req.session.username,
+		role: req.session.role,
+		submissions: allSubs,
+		ass_info: ass_info[0],
+		sub_info: subData[0],
+		team_code: team_code,
+		team_name: team_name,
+		team_desc: team_desc,
+	};
+	if (req.session.notification) {
+		context.notification = req.session.notification;
+		req.session.notification = undefined;
+	}
+	res.render("grading", context);
 });
 
 router.route("/download/:ass_id").get(auth.authMiddleware, async (req, res) => {
-    const ass_id = req.params.ass_id;
-    const ass_info = await assignmentDB.getAssignmentById(ass_id);
-    if (ass_info.length === 0) {
-        res.redirect("/");
-        return;
-    }
+	const ass_id = req.params.ass_id;
+	const ass_info = await assignmentDB.getAssignmentById(ass_id);
+	if (ass_info.length === 0) {
+		res.redirect("/");
+		return;
+	}
 
-    const team_id = ass_info[0].TEAM_ID;
-    const team_info = await teams.getTeamInfo(team_id);
+	const team_id = ass_info[0].TEAM_ID;
+	const team_info = await teams.getTeamInfo(team_id);
 
-    if (team_info.length === 0) {
-        res.redirect("/");
-        return;
-    }
+	if (team_info.length === 0) {
+		res.redirect("/");
+		return;
+	}
 
-    const team_role = await teams.checkUserInTeam(req.session.user_id);
-    if (team_role.length === 0 || team_role[0].ROLE === "student") {
-        res.redirect("/");
-        return;
-    }
+	const team_role = await teams.checkUserInTeam(req.session.user_id);
+	if (team_role.length === 0 || team_role[0].ROLE === "student") {
+		res.redirect("/");
+		return;
+	}
 
-    let allSubs = await assignmentDB.allSubs(ass_id);
-    let fileList = [];
-    for (let i = 0; i < allSubs.length; i++) {
-        if (allSubs[i].SUBMISSION_FILE)
-            fileList.push(allSubs[i].SUBMISSION_FILE);
-    }
-    let result = await compression.createZipArchive(fileList);
-    res.redirect("/file/" + result);
+	let allSubs = await assignmentDB.allSubs(ass_id);
+	let fileList = [];
+	for (let i = 0; i < allSubs.length; i++) {
+		if (allSubs[i].SUBMISSION_FILE)
+			fileList.push(allSubs[i].SUBMISSION_FILE);
+	}
+	let result = await compression.createZipArchive(fileList);
+	res.redirect("/file/" + result);
 });
 
 router
-    .route("/submit/:ass_id/:std_id")
-    .post(auth.authMiddleware, async (req, res) => {
-        const ass_id = req.params.ass_id;
-        const std_id = req.params.std_id;
+	.route("/submit/:ass_id/:std_id")
+	.post(auth.authMiddleware, async (req, res) => {
+		const ass_id = req.params.ass_id;
+		const std_id = req.params.std_id;
 
-        const ass_info = await assignmentDB.getAssignmentById(ass_id);
-        if (ass_info.length === 0) {
-            res.redirect("/");
-            return;
-        }
+		const ass_info = await assignmentDB.getAssignmentById(ass_id);
+		if (ass_info.length === 0) {
+			res.redirect("/");
+			return;
+		}
 
-        const team_id = ass_info[0].TEAM_ID;
-        const team_info = await teams.getTeamInfo(team_id);
+		const team_id = ass_info[0].TEAM_ID;
+		const team_info = await teams.getTeamInfo(team_id);
 
-        if (team_info.length === 0) {
-            res.redirect("/");
-            return;
-        }
+		if (team_info.length === 0) {
+			res.redirect("/");
+			return;
+		}
 
-        const team_role = await teams.checkUserInTeam(req.session.user_id, team_id);
-        console.log(team_role);
-        if (team_role.length === 0 || team_role[0].ROLE === "student") {
-            res.redirect("/");
-            return;
-        }
+		const team_role = await teams.checkUserInTeam(
+			req.session.user_id,
+			team_id
+		);
+		console.log(team_role);
+		if (team_role.length === 0 || team_role[0].ROLE === "student") {
+			res.redirect("/");
+			return;
+		}
 
-        const score = req.body.score;
+		const score = req.body.score;
 
-        await assignmentDB.gradeSubmission(ass_id, std_id, score);
-        res.redirect("/grading/" + ass_id);
-    });
+		await assignmentDB.gradeSubmission(ass_id, std_id, score);
+		req.session.notification = {
+			status: "is-success is-light",
+			content: "Successfully Submitted",
+		};
+		res.redirect("/grading/" + ass_id);
+	});
 
 module.exports = {
-    router,
+	router,
 };
